c6c38dfcd7a7ce22938279997fd3592123b7dd10
Robert O'Shea <robertosheagamedev@gmail.com>
2026-02-08 01:56:45 +0000

    Add basic player controller and physics

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
+mod character_controller;
+
+use avian2d::{math::*, prelude::*};
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
 
+use character_controller::*;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins((
+            DefaultPlugins.set(AssetPlugin {
+                // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                // This causes errors and even panics in web builds on itch.
+                // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                meta_check: AssetMetaCheck::Never,
+                ..default()
+            }),
+            PhysicsPlugins::default(),
+            CharacterControllerPlugin,
+        ))
+        .insert_resource(ClearColor(Color::srgb(0.05, 0.05, 0.05)))
+        .insert_resource(Gravity(Vector::NEG_Y * 1000.0))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2d);
-    commands.spawn(Sprite {
-        image: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("ducky.png"),
+            ..Default::default()
+        },
+        CharacterControllerBundle::new(Collider::capsule(50.0, 100.0)).with_movement(
+            1250.0,
+            5.0,
+            400.0,
+            (30.0 as Scalar).to_radians(),
+        ),
+        Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
+        Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+        GravityScale(1.5),
+        ColliderDensity(2.0),
+        Transform::from_xyz(0.0, -100.0, 0.0),
+    ));
+    // Platforms
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(0.7, 0.7, 0.8),
+            custom_size: Some(Vec2::new(1100.0, 50.0)),
+            ..default()
+        },
+        Transform::from_xyz(0.0, -175.0, 0.0),
+        RigidBody::Static,
+        Collider::rectangle(1100.0, 50.0),
+    ));
 }
